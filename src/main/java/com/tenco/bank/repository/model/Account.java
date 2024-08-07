f1efbb037ab46f41690962caff2be258ec16aae0
CYJ <wjdtpdbs1887@naver.com>
2024-08-07 12:44:32 +0900

    retry

---
+++ src/main/java/com/tenco/bank/repository/model/Account.java
@@ -0,0 +1,23 @@
+package com.tenco.bank.repository.model;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Account {
+	private Integer id;
+	private String number;
+	private String password;
+	private Long balance;
+	private Integer userId;
+	private Timestamp createdAt;
+}
